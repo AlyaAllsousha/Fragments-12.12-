281ae1e8babaa8c30404f54af13b794930e88f87
student <albisharafiova@yandex.ru>
2024-12-12 15:20:53 +0300

    s

--- app/src/main/java/ru/example/a1212/MainActivity.kt
+++ app/src/main/java/ru/example/a1212/MainActivity.kt
@@ -6,9 +6,30 @@ import androidx.databinding.DataBindingUtil
 import ru.example.a1212.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    var state:Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding:ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setContentView(binding.root)
+        binding.btn.setOnClickListener {
+            when(state){
+                0 -> {
+                    val fragment = BlankFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.FrameLayout, fragment).commit()
+                }
+                1  ->{
+                    val fragment2 = BlankFragment2()
+                    supportFragmentManager.beginTransaction().replace(R.id.FrameLayout, fragment2).commit()
+                }
+                2 ->{
+                    val fragment3 = SettingsFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.FrameLayout, fragment3).commit()
+                }
+                else ->{
+
+                }
+            }
+            state = (state +1) % 3
+        }
     }
 }
